5dc27a09f2979a73baeb507f5e46bb3a8a3621d3
Yanelly Mego <89563053+yanellymego@users.noreply.github.com>
2025-05-04 16:35:42 -0700

    Starting xcode file

---
+++ WillowAndFinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WillowAndFinchApp.swift
+//  WillowAndFinch
+//
+//  Created by Yanelly Mego on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WillowAndFinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

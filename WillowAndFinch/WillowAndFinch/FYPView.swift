f02fb08e6a3af39322f4aa40de08d8f4569961db
Yanelly Mego <mego@chapman.edu>
2025-05-04 13:26:59 -0700

    Added view page

---
+++ WillowAndFinch/WillowAndFinch/FYPView.swift
@@ -0,0 +1,18 @@
+//
+//  FYPView.swift
+//  WillowAndFinch
+//
+//  Created by Yanelly Mego on 5/4/25.
+//
+
+import SwiftUI
+
+struct FYPView: View {
+    var body: some View {
+        Text("Hello people")
+    }
+}
+
+#Preview {
+    FYPView()
+}

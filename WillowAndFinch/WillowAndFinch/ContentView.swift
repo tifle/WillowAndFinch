e68ed557e25346a2fa5d10ac78110f52fc38f599
Yanelly Mego <mego@chapman.edu>
2025-05-04 13:22:53 -0700

    Empty xcode

---
+++ WillowAndFinch/WillowAndFinch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WillowAndFinch
+//
+//  Created by Yanelly Mego on 5/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Testing testing one two three")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

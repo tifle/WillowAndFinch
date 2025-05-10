7e4f1850414b4ba9df9b00e1bc1feb059ce8a5c0
Tiffany Le <tifle@chapman.edu>
2025-05-09 21:15:33 -0700

    Add logo

--- WillowAndFinch/WillowAndFinch/HomeView.swift
+++ WillowAndFinch/WillowAndFinch/HomeView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("This is the home page")
+        // Navigation View
+        NavigationView {
+            // Vertical Stack
+            VStack(alignment: .leading) {
+                // Logo / Home Page
+                HStack {
+                    Spacer()
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 50)
+                    Spacer()
+                }
+                .padding(.top)
+                Text("Welcome to Willow & Finch")
+                    .font(.custom("Times New Roman", size: 28))
+                    .bold()
+                    .padding(.bottom)
+                
+                // search bar
+                TextField("Search by book title", text: .constant(""))
+                    .font(.custom("Times New Roman", size: 16))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.bottom)
+                
+                Text("Recommended")
+                    .font(.custom("Times New Roman", size: 18))
+                ScrollView(.horizontal) {
+                    // show the top 5 recommended books
+//                    HStack(0..<5) { _ in BookDetailView(title: "Recommended Book")
+//                        
+//                    }
+                }
+            }
+        }
     }
 }
 

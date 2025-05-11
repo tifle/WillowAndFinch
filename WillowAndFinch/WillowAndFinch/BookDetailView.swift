89267b8918ccaab58dead189f5351c44f809f47c
Yanelly Mego <89563053+yanellymego@users.noreply.github.com>
2025-05-11 13:09:04 -0700

    General Book Overview Page added

--- WillowAndFinch/WillowAndFinch/BookDetailView.swift
+++ WillowAndFinch/WillowAndFinch/BookDetailView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct BookDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.latteMilk.ignoresSafeArea(edges: .bottom)
+            Color.warmBeige.ignoresSafeArea(edges: .top)
+            
+                        
+            VStack{
+                Image("DefaultCover")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .frame(width: 200)
+                
+                    .padding(50)
+                
+                Text("Title")
+                    .bold()
+                Text("Author")
+                
+//enter regular book.csv file to get this info + change cover from default
+                ScrollView{
+                    Spacer(minLength: 30)
+                    Text("Potentially Summary?")
+                    Text("Year of publication")
+                    Text("Publisher")
+                }
+            }
+        }
     }
 }
 
--- WillowAndFinch/WillowAndFinch/HomeView.swift
+++ WillowAndFinch/WillowAndFinch/HomeView.swift
@@ -10,40 +10,47 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         // Navigation View
-        NavigationView {
-            // Vertical Stack
-            VStack(alignment: .leading) {
-                // Logo / Home Page
-                HStack {
-                    Spacer()
-                    Image("logo")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 50)
-                    Spacer()
-                }
-                .padding(.top)
-                Text("Welcome to Willow & Finch")
-                    .font(.custom("Didot", size: 28))
-                    .bold()
-                    .padding(.bottom)
+            NavigationView {
+                // Vertical Stack
+                VStack(alignment: .leading) {
+                    // Logo / Home Page
+                    HStack {
+                        Spacer()
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 50)
+                        Spacer()
+                    }
+                    .padding(.top)
+                    Text("Welcome to Willow & Finch")
+                        .font(.custom("Didot", size: 28))
+                    //                    .bold()
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.bottom)
+                    
+                    // search bar
+                    TextField("Search by book title", text: .constant(""))
+                        .font(.custom("Didot", size: 16))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: 380)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.bottom)
+                    
+                    Text("Recommended")
+                        .font(.custom("Didot", size: 18))
+                    ScrollView(.horizontal) {
+                        // show the top 5 recommended books
+                        //                    HStack(0..<5) { _ in BookDetailView(title: "Recommended Book")
+                        //                        
+                    //                    }
+                    } //end of scroll view
+            } //end of VStack
+                .background(Color.sageGreen.ignoresSafeArea(edges: .top))
+                .background(Color.latteMilk)
+
                 
-                // search bar
-                TextField("Search by book title", text: .constant(""))
-                    .font(.custom("Didot", size: 16))
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.bottom)
-                
-                Text("Recommended")
-                    .font(.custom("Didot", size: 18))
-                ScrollView(.horizontal) {
-                    // show the top 5 recommended books
-//                    HStack(0..<5) { _ in BookDetailView(title: "Recommended Book")
-//                        
-//                    }
-                }
-            }
-        }
+        } //end of navigation view
     }
 }
 
